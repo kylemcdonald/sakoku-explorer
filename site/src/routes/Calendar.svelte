3c290075b38b40f8bce6f388084da0ff10a8dc79
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-11-02 14:43:31 -0800

    adding fullcalendar

--- site/src/routes/Calendar.svelte
+++ site/src/routes/Calendar.svelte
@@ -1,2 +1,40 @@
+<script>
+  import { onMount } from 'svelte';
+  import { Calendar } from '@fullcalendar/core';
+  import dayGridPlugin from '@fullcalendar/daygrid';
+  import timeGridPlugin from '@fullcalendar/timegrid';
+
+	onMount(async () => {
+    let today = new Date().toISOString().split('T')[0];
+    let elt = document.querySelector('#calendar');
+    const calendar = new Calendar(elt, {
+    plugins: [
+      dayGridPlugin,
+      timeGridPlugin
+    ],
+    aspectRatio: 2,
+    eventDidMount: function(info) {
+      const tooltip = new Tooltip(info.el, {
+        title: info.event.title,
+        placement: 'top',
+        trigger: 'hover',
+        container: 'body'
+      });
+    },
+    scrollTime: '9:00AM',
+    initialView: 'timeGridWeek',
+    initialDate: today,
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    }
+    });
+    calendar.render();
+	});
+
+</script>
+
 <h1>Calendar</h1>
 <p>Let's go!</p>
+<div id='calendar'></div>
