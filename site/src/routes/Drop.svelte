44f5dbb6bb759b31ae16793ed0c86386bd04808b
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-11-02 23:52:57 -0800

    css

--- site/src/routes/Drop.svelte
+++ site/src/routes/Drop.svelte
@@ -18,6 +18,25 @@
   .right-col {
     margin-left: 2em;
   }
+
+  h1 {
+    font-size: 1em;
+  }
+
+  p {
+    font-size: 0.8em;
+  }
+
+  #arrow {
+    width: 0.6em;
+  }
+  #drop-text {
+    width: calc(100% - 1.3em);
+    margin-left: 0.6em;
+    float: right;
+  }
+
+
 </style>
 
 <div class="center-block center-block-flex">
@@ -28,10 +47,13 @@
     <Dropzone
       on:drop={handleFilesSelect}
       accept=""
-      containerStyles="border: 2px dotted black; margin-bottom: 1em">
+      containerStyles="border: 2px dotted black; background: white; margin-bottom: 1em; cursor: pointer;">
       <p/> <!-- Google Facebook -->
     </Dropzone>
-    <h1>{$_('drop.header')}</h1>
-    <p>{$_('drop.description')}</p>
+    <img id='arrow' src='../assets/arrow.svg' alt=''>
+    <div id='drop-text'>
+      <h1>{$_('drop.header')}</h1>
+      <p>{$_('drop.description')}</p>
+    </div>
   </div>
 </div>
