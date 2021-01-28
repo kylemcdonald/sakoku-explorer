b870ed86f515c84dc92e737a57d7b18df7472bef
Kyle McDonald <kyle@kylemcdonald.net>
2021-01-28 13:50:49 -0800

    disclaimer. closes #38

--- site/src/routes/Home.svelte
+++ site/src/routes/Home.svelte
@@ -2,6 +2,18 @@
   import { _ } from "../i18n.js";
 </script>
 
+<div class="center-block center-block-flex">
+  <div class="left-col">
+    <img src="../assets/about.svg" alt={$_("about.about-alt")} />
+  </div>
+  <div class="right-col">
+    <h1>{$_("about.header")}</h1>
+    <p>{$_("about.description-1")}</p>
+    <p>{$_("about.description-2")}</p>
+    <p class="disclaimer">{$_("about.disclaimer")}</p>
+  </div>
+</div>
+
 <style>
   .center-block {
     text-align: left;
@@ -10,15 +22,10 @@
   .right-col {
     margin-left: 2em;
   }
-</style>
 
-<div class='center-block center-block-flex'>
-  <div class='left-col'>
-    <img src='../assets/about.svg' alt='{$_('about.about-alt')}'>
-  </div>
-  <div class='right-col'>
-    <h1>{$_('about.header')}</h1>
-    <p>{$_('about.description-1')}</p>
-    <p>{$_('about.description-2')}</p>
-  </div>
-</div>
+  .disclaimer {
+    font-size: 0.6em;
+    text-transform: uppercase;
+    text-align: justify;
+  }
+</style>
